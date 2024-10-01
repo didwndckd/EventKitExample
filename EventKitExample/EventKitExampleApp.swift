bb9c8aab2c8abe808413cf4676d3dd264b8d8525
didwndckd <didwndckd@gmail.com>
2024-10-01 23:03:17 +0900

    Initial Commit

---
+++ EventKitExample/EventKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventKitExampleApp.swift
+//  EventKitExample
+//
+//  Created by yjc on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct EventKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
